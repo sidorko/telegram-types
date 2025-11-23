9978f99e4983113c5417d72a36985e79a84a7923
sidorko <kirill.sidorko@ya.ru>
2025-11-24 01:24:13 +0300

    add back button

--- index.d.ts
+++ index.d.ts
@@ -282,7 +282,45 @@ export interface SafeAreaInset {}
 
 export interface ContentSafeAreaInset {}
 
-export interface BackButton {}
+/**
+ * This object controls the **back** button, which can be displayed in the header of the Mini App in the Telegram interface.
+ */
+export interface BackButton {
+  /**
+   * Shows whether the button is visible. Set to *false* by default.
+   */
+  isVisible: boolean;
+
+  /**
+   * A method that sets the button press event handler.
+   * An alias for `Telegram.WebApp.onEvent('backButtonClicked', callback)`
+   *
+   * @since Bot API 6.1+
+   */
+  onClick(callback: () => void): BackButton;
+
+  /**
+   * A method that removes the button press event handler.
+   * An alias for `Telegram.WebApp.onEvent('backButtonClicked', callback)`
+   *
+   * @since Bot API 6.1+
+   */
+  offClick(callback: () => void): BackButton;
+
+  /**
+   * A method to make the button active and visible.
+   *
+   * @since Bot API 6.1+
+   */
+  show(): BackButton;
+
+  /**
+   * A method to hide the button.
+   *
+   * @since Bot API 6.1+
+   */
+  hide(): BackButton;
+}
 
 export interface BottomButton {}
 
